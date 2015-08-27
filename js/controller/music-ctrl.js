a0432e548a3b363c4cd9d3ad6bdedfe3c35f6e05
Tony <igitinode@gmail.com>
2015-08-27 18:00:41 +0800

    #2 实现点击播放音乐

---
+++ js/controller/music-ctrl.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('siteJack', []);
+
+angular.module('siteJack')
+.controller('MusicCtrl', function ($scope) {
+  $scope.start = true;
+  var _audio = new Audio();
+  _audio.src = 'http://7xlcxw.com1.z0.glb.clouddn.com/home.mp3';
+  _audio.onended = function () {
+    $scope.start = !$scope.start;
+    $scope.$apply();  // dirty data fresh
+  };
+  $scope._audio = _audio;
+  $scope._audio.play();
+
+  $scope.play = function () {
+    if ($scope.start){
+      $scope._audio.pause();
+    } else {
+       $scope._audio.play();
+    }
+    $scope.start = !$scope.start;
+  };
+});
